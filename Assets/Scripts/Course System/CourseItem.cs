e4b31008a5852396dd8bf2dbef5dab65930cf996
Brinley Zhao <brinleyzhao@gmaill.com>
2021-06-09 15:08:56 -0400

    course scriptable objects: 4 stats change slots

--- Assets/Scripts/Course System/CourseItem.cs
+++ Assets/Scripts/Course System/CourseItem.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Player;
 using UnityEngine;
 
@@ -8,26 +9,32 @@ namespace Course_System
   /// A ScriptableObject that represents any course
   /// </summary>
   [CreateAssetMenu(fileName = "new course", menuName = "Scriptables/newCourse")]
-  public class CourseItem : ScriptableObject, ISerializationCallbackReceiver
+  public class CourseItem : ScriptableObject
   {
 
     // CONFIG DATA
-    [Tooltip("Auto-generated UUID for saving/loading. Clear this field if you want to generate a new one.")]
-    [SerializeField] string itemId = null;
+    // [Tooltip("Auto-generated UUID for saving/loading. Clear this field if you want to generate a new one.")]
+    // [SerializeField] string itemId = null;
 
     // [Tooltip("The UI icon to represent this item in the inventory.")]
     // [SerializeField] Sprite icon = null;
-    [SerializeField] string courseText = null;
+    // [SerializeField] string courseText = null;
 
 
-    [Tooltip("Course name to be displayed in UI.")]
-    public string courseName;
+    // [Tooltip("Course name to be displayed in UI.")]
+    // public string courseName;
 
     [Tooltip("Course description to be displayed in UI.")]
     [SerializeField][TextArea] string description = null;
 
-    //public int classLevel;
-    public List<Stats> listOfStats; //len 3
+    // public Dictionary<Stats, int> statsChange;
+
+    [Serializable]
+    public struct StatChange {
+      public Stats stat;
+      public int valueChange;
+    }
+    public StatChange[] statsChange = new StatChange[4];
 
 
 
@@ -45,66 +52,66 @@ namespace Course_System
         /// <returns>
         /// Inventory item instance corresponding to the ID.
         /// </returns>
-        public static CourseItem GetFromId(string itemId)
-        {
-            if (itemLookupCache == null)
-            {
-                itemLookupCache = new Dictionary<string, CourseItem>();
-                var itemList = Resources.LoadAll<CourseItem>("");
-                foreach (var item in itemList)
-                {
-                    if (itemLookupCache.ContainsKey(item.itemId))
-                    {
-                        Debug.LogError(
-                          $"Looks like there's a duplicate GameDevTV.UI.InventorySystem ID for objects: {itemLookupCache[item.itemId]} and {item}");
-                        continue;
-                    }
-
-                    itemLookupCache[item.itemId] = item;
-                }
-            }
-
-            if (itemId == null || !itemLookupCache.ContainsKey(itemId)) return null;
-            return itemLookupCache[itemId];
-        }
+        // public static CourseItem GetFromId(string itemId)
+        // {
+        //     if (itemLookupCache == null)
+        //     {
+        //         itemLookupCache = new Dictionary<string, CourseItem>();
+        //         var itemList = Resources.LoadAll<CourseItem>("");
+        //         foreach (var item in itemList)
+        //         {
+        //             if (itemLookupCache.ContainsKey(item.itemId))
+        //             {
+        //                 Debug.LogError(
+        //                   $"Looks like there's a duplicate GameDevTV.UI.InventorySystem ID for objects: {itemLookupCache[item.itemId]} and {item}");
+        //                 continue;
+        //             }
+        //
+        //             itemLookupCache[item.itemId] = item;
+        //         }
+        //     }
+        //
+        //     if (itemId == null || !itemLookupCache.ContainsKey(itemId)) return null;
+        //     return itemLookupCache[itemId];
+        // }
 
         /*public Sprite GetIcon()
         {
             return icon;
         }*/
 
-        public string GetItemID()
-        {
-            return itemId;
-        }
+        // public string GetItemID()
+        // {
+        //     return itemId;
+        // }
+        //
+        //
+        // public string GetCourseName()
+        // {
+        //     return courseName;
+        // }
 
-
-        public string GetCourseName()
-        {
-            return courseName;
-        }
-
-        public string GetDescription()
-        {
-            return description;
-        }
+        // public string GetDescription()
+        // {
+        //     return description;
+        // }
 
         // PRIVATE
 
-        void ISerializationCallbackReceiver.OnBeforeSerialize()
-        {
-            // Generate and save a new UUID if this is blank.
-            if (string.IsNullOrWhiteSpace(itemId))
-            {
-                itemId = System.Guid.NewGuid().ToString();
-            }
-        }
+        // void ISerializationCallbackReceiver.OnBeforeSerialize()
+        // {
+        //     // Generate and save a new UUID if this is blank.
+        //     if (string.IsNullOrWhiteSpace(itemId))
+        //     {
+        //         itemId = System.Guid.NewGuid().ToString();
+        //     }
+        // }
 
-        void ISerializationCallbackReceiver.OnAfterDeserialize()
-        {
-            // Require by the ISerializationCallbackReceiver but we don't need
-            // to do anything with it.
-        }
+        // void ISerializationCallbackReceiver.OnAfterDeserialize()
+        // {
+        //     // Require by the ISerializationCallbackReceiver but we don't need
+        //     // to do anything with it.
+        // }
     }
 }
 
--- Assets/Scripts/Player/StatsTypeEnum.cs
+++ Assets/Scripts/Player/StatsTypeEnum.cs
@@ -7,8 +7,6 @@
     Strength, //2
     Magic, //4
     Intelligence, //5
-    Gardening, //6
-    Spell, //7
     Dexterity, //8
     Darkness, //9
     Perception,
